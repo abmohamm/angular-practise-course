5a535ef095a50b63cf2976d6298dc80149fdda62
abmohamm <mohd.92java@gmail.com>
2019-11-29 20:11:09 -0500

    Adding changes to support Drop down values

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,28 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { MatInputModule,MatCardModule,MatButtonModule } from '@angular/material';
+import { MatInputModule,MatCardModule,
+         MatButtonModule,MatToolbarModule,
+         MatExpansionModule } from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CreatePostsComponent } from './posts/create-posts/create-posts.component';
 import { HeaderComponent } from './headers/header/header.component';
+import { PostsListComponent } from './posts/posts-list/posts-list/posts-list.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     CreatePostsComponent,
-    HeaderComponent
+    HeaderComponent,
+    PostsListComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    BrowserAnimationsModule,
-    MatInputModule,
-    MatCardModule,
-    MatButtonModule
+    BrowserModule,AppRoutingModule,FormsModule,BrowserAnimationsModule,
+    MatInputModule,MatCardModule,MatButtonModule,MatToolbarModule,MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
