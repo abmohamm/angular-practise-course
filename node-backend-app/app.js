86cdb1fa5ca91387d4aa5a4dcbfd03879a2679ec
abmohamm <mohd.92java@gmail.com>
2019-12-07 15:46:10 -0500

    Adding changes to support search(GET) Operation

--- node-backend-app/app.js
+++ node-backend-app/app.js
@@ -2,13 +2,20 @@ const express = require('express');
 
 const app = express();
 
-app.use((request,response,next) => {
-  console.log('<===first middle wares===>');
-  next();
-});
+app.use('/api/employees', (request,response,next) => {
+    const employees = [
+            {firstname: 'Steven', lastname: 'King', email: 'sking@gmail.com', jobid: 'AD_PRES', description: 'Program Manager'},
+            {firstname: 'Neena', lastname: 'Kochhar', email: 'nkochhar@gmail.com', jobid: 'AD_VP', description: 'Application Developer'},
+            {firstname: 'Lex', lastname: 'De haan', email: 'ldehaan@gmail.com', jobid: 'AD_VP', description: 'Vice President'},
+            {firstname: 'Alexander', lastname: 'Hunold', email: 'ahunold@gmail.com', jobid: 'IT_PROG', description: 'QA Tester'},
+            {firstname: 'Bruce', lastname: 'Ernst', email: 'bernst@gmail.com', jobid: 'AD_PRES', description: 'Data Analyst'}
+    ];
+    response.status(200).json({
+        message: "Records retrieved successfully",
+        status: "OK",
+        employees: employees
 
-app.use((request,response,next) => {
-    response.send('Hello from express js !!! :::::');
+    });
 });
 
 module.exports = app;
