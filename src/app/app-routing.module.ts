7093986e27dfa2f27d111195d8818e2437a95cf0
abmohamm <mohd.92java@gmail.com>
2019-12-27 17:30:59 -0500

    Added changes to support Login Authentication

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { EmployeesListComponent } from './employees/employees-list/employees-list.component';
 import { CreateEmployeesComponent } from './employees/create-employees/create-employees.component';
+import { LoginComponent } from './auth/login/login.component';
 
 const routes: Routes = [
   { path: '', component: EmployeesListComponent },  // http://localhost:4200/
   { path: 'create', component: CreateEmployeesComponent},  // http://localhost:4200/create
-  { path: 'edit/:employeeId', component: CreateEmployeesComponent}
+  { path: 'edit/:employeeId', component: CreateEmployeesComponent}, // http://localhost:4200/edit/{}
+  { path: 'login', component: LoginComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { MatInputModule,  MatCardModule,
          MatButtonModule, MatToolbarModule,
          MatExpansionModule, MatProgressSpinnerModule,
@@ -14,6 +14,8 @@ import { HeaderComponent } from './headers/header/header.component';
 import { CreateEmployeesComponent } from './employees/create-employees/create-employees.component';
 import { EmployeesListComponent } from './employees/employees-list/employees-list.component';
 import { EmployeesService } from './employees/employees.service';
+import { LoginComponent } from './auth/login/login.component';
+import { SignUpComponent } from './auth/signup/sign-up/sign-up.component';
 
 
 @NgModule({
@@ -21,10 +23,12 @@ import { EmployeesService } from './employees/employees.service';
     AppComponent,
     CreateEmployeesComponent,
     HeaderComponent,
-    EmployeesListComponent
+    EmployeesListComponent,
+    LoginComponent,
+    SignUpComponent
   ],
   imports: [
-    BrowserModule,  AppRoutingModule, ReactiveFormsModule,  BrowserAnimationsModule,
+    BrowserModule,  AppRoutingModule, ReactiveFormsModule, FormsModule, BrowserAnimationsModule,
     MatInputModule, MatCardModule, MatButtonModule, MatToolbarModule,
     MatExpansionModule, MatProgressSpinnerModule, MatPaginatorModule, HttpClientModule
   ],
