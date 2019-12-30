7726d50ab192ec385c99b74ce77f7fd7249e55e5
abmohamm <mohd.92java@gmail.com>
2019-12-30 12:41:10 -0500

    Added changes to support guard against create/edit components when not lopgged-in

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,18 +4,20 @@ import { EmployeesListComponent } from './employees/employees-list/employees-lis
 import { CreateEmployeesComponent } from './employees/create-employees/create-employees.component';
 import { LoginComponent } from './auth/login/login.component';
 import { SignUpComponent } from './auth/signup/sign-up.component';
+import { AuthGuard } from './auth/auth-guard';
 
 
 const routes: Routes = [
   { path: '', component: EmployeesListComponent },  // http://localhost:4200/
-  { path: 'create', component: CreateEmployeesComponent },  // http://localhost:4200/create
-  { path: 'edit/:employeeId', component: CreateEmployeesComponent }, // http://localhost:4200/edit/{}
+  { path: 'create', component: CreateEmployeesComponent, canActivate: [AuthGuard] },  // http://localhost:4200/create
+  { path: 'edit/:employeeId', component: CreateEmployeesComponent, canActivate: [AuthGuard] }, // http://localhost:4200/edit/{}
   { path: 'login', component: LoginComponent }, // http://localhost:4200/login
   { path: 'signup', component: SignUpComponent }  // http://localhost:4200/signup
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [AuthGuard]
 })
 export class AppRoutingModule { }
