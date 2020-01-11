e78a9e16b86054dc9a0ce5ee30f837f2354ef54c
abmohamm <mohd.92java@gmail.com>
2020-01-11 08:20:02 -0500

    Adding support to redirect API requests to controllers

---
+++ node-backend-app/controllers/employees.js
@@ -0,0 +1,147 @@
+const Employee = require("../models/employee");
+
+
+exports.createEmployee = (request, response, next) => {
+  //  const employee = request.body;
+  const url = request.protocol + '://' + request.get("host");
+  const employee = new Employee({
+    firstname: request.body.firstname,
+    lastname: request.body.lastname,
+    email: request.body.email,
+    jobid: request.body.jobid,
+    description: request.body.description,
+    imagePath: url + "/images" + request.file.filename,
+    creator: request.userData.userId
+  });
+  console.log("Employee Details : " + employee);
+  employee.save()   // This Command will insert a document into database.
+    .then(createdEmployee => {
+      response.status(201).json({
+        message: "Records stored successfully",
+        status: "OK",
+        employee: {
+          // ...createdEmployee,
+          employeeId: createdEmployee.employeeId,
+          empFirstName: createdEmployee.empFirstName,
+          empLastName: createdEmployee.empLastName,
+          empEmailId: createdEmployee.empEmailId,
+          description: createdEmployee.description,
+          imagePath: createdEmployee.imagePath
+        }
+      });
+    })
+    .catch(error => {
+      response.status(500).json({
+        message: 'Adding Information Process Failed!!!'
+      });
+    });
+}
+
+exports.updateEmployee = (request, response, next) => {
+  let imagePath = request.body.imagePath;
+  let url;
+  if (request.file) {
+    url = request.protocol + '://' + request.get("host");
+    imagePath = url + "/images" + request.file.filename;
+  }
+  const employee = new Employee({
+    firstname: request.body.firstname,
+    lastname: request.body.lastname,
+    email: request.body.email,
+    jobid: request.body.jobid,
+    description: request.body.description,
+    imagePath: imagePath,
+    creator: request.userData.userId
+  });
+  Employee.updateOne({ _id: request.params.employeeId, creator: request.userData.userId }, employee)
+    .then(result => {
+      console.log("Employee Details : " + result);
+      if (result.nModified > 0) {
+        response.status(200).json({
+          message: "Records Updated Successfully",
+          status: "OK"
+        });
+      }
+      else {
+        response.status(401).json({
+          message: "Not Authorized to Edit/update",
+          status: "BAD AUTHENTICATION REQUEST"
+        });
+      }
+    })
+    .catch(error => {
+      response.status(500).json({
+        message: 'Updating Information Process Failed!!!'
+      });
+    });
+}
+
+exports.getEmployees = (request, response, next) => {
+  const pageSize = +request.query.pagesize;
+  const currentPage = +request.query.page;
+  const employeeQuery = Employee.find();
+  let fetchedEmployees;
+  if (pageSize && currentPage) {
+    employeeQuery.skip(pageSize * (currentPage - 1))
+      .limit(pageSize);
+  }
+  employeeQuery.then((documents) => {
+    console.log("Data Retrieved : " + documents);
+    fetchedEmployees = documents;
+    return Employee.count();
+  }).then(count => {
+    response.status(200).json({
+      message: "Records retrieved successfully",
+      status: "OK",
+      employees: fetchedEmployees,
+      maxEmployees: count
+    });
+  })
+    .catch(error => {
+      response.status(500).json({
+        message: 'Fetching Information Process Failed!!!'
+      });
+    });
+}
+
+exports.getEmployee = (request, response, next) => {
+  Employee.findById(request.params.employeeId)
+    .then(employee => {
+      if (employee) {
+        response.status(200).json(employee);
+      } else {
+        response.status(404).json({
+          message: "Records Not Found"
+        });
+      }
+    })
+    .catch(error => {
+      response.status(500).json({
+        message: 'Fetching Information Process Failed!!!'
+      });
+    });
+}
+
+exports.deleteEmployee = (request, response, next) => {
+  console.log("Employee Id : " + request.params.employeeId);
+  Employee.deleteOne({ _id: request.params.employeeId, creator: request.userData.userId })
+    .then(result => {
+      if (result.n > 0) {
+        response.status(200).json({
+          message: "Record Deleted Successfully",
+          status: "OK"
+        });
+      }
+      else {
+        response.status(401).json({
+          message: "Not Authorized to Delete",
+          status: "BAD AUTHENTICATION REQUEST"
+        });
+      }
+    })
+    .catch(error => {
+      response.status(500).json({
+        message: 'Deleting Information Process Failed!!!'
+      });
+    });
+}
--- node-backend-app/routes/employees.js
+++ node-backend-app/routes/employees.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const multer = require("multer");
-
-const Employee = require("../models/employee");
 const checkAuth = require("../middle-ware/check-auth");
-
 const router = express.Router();
+const employeeController = require("../controllers/employees");
 
 const MIME_TYPE_MAP = {
   'image/png': 'png',
@@ -29,152 +27,17 @@ const storage = multer.diskStorage({
 });
 
 
-router.post("", checkAuth, multer({ storage: storage }).single("image"), (request, response, next) => {
-  //  const employee = request.body;
-  const url = request.protocol + '://' + request.get("host");
-  const employee = new Employee({
-    firstname: request.body.firstname,
-    lastname: request.body.lastname,
-    email: request.body.email,
-    jobid: request.body.jobid,
-    description: request.body.description,
-    imagePath: url + "/images" + request.file.filename,
-    creator: request.userData.userId
-  });
-  console.log("Employee Details : " + employee);
-  employee.save()   // This Command will insert a document into database.
-    .then(createdEmployee => {
-      response.status(201).json({
-        message: "Records stored successfully",
-        status: "OK",
-        employee: {
-          // ...createdEmployee,
-          employeeId: createdEmployee.employeeId,
-          empFirstName: createdEmployee.empFirstName,
-          empLastName: createdEmployee.empLastName,
-          empEmailId: createdEmployee.empEmailId,
-          description: createdEmployee.description,
-          imagePath: createdEmployee.imagePath
-        }
-      });
-    })
-    .catch(error => {
-      response.status(500).json({
-        message: 'Adding Information Process Failed!!!'
-      });
-    });
-});
+router.post("", checkAuth,
+                multer({ storage: storage }).single("image"), employeeController.createEmployee);
 
-router.put("/:employeeId", checkAuth, multer({ storage: storage }).single("image"), (request, response, next) => {
-  let imagePath = request.body.imagePath;
-  let url;
-  if (request.file) {
-    url = request.protocol + '://' + request.get("host");
-    imagePath = url + "/images" + request.file.filename;
-  }
-  const employee = new Employee({
-    firstname: request.body.firstname,
-    lastname: request.body.lastname,
-    email: request.body.email,
-    jobid: request.body.jobid,
-    description: request.body.description,
-    imagePath: imagePath,
-    creator: request.userData.userId
-  });
-  Employee.updateOne({ _id: request.params.employeeId, creator: request.userData.userId }, employee)
-    .then(result => {
-      console.log("Employee Details : " + result);
-      if (result.nModified > 0) {
-        response.status(200).json({
-          message: "Records Updated Successfully",
-          status: "OK"
-        });
-      }
-      else {
-        response.status(401).json({
-          message: "Not Authorized to Edit/update",
-          status: "BAD AUTHENTICATION REQUEST"
-        });
-      }
-    })
-    .catch(error => {
-      response.status(500).json({
-        message: 'Updating Information Process Failed!!!'
-      });
-    });
-});
+router.put("/:employeeId", checkAuth,
+                           multer({ storage: storage }).single("image"), employeeController.updateEmployee);
 
-router.get("", (request, response, next) => {
-  const pageSize = +request.query.pagesize;
-  const currentPage = +request.query.page;
-  const employeeQuery = Employee.find();
-  let fetchedEmployees;
-  if (pageSize && currentPage) {
-    employeeQuery.skip(pageSize * (currentPage - 1))
-      .limit(pageSize);
-  }
-  employeeQuery.then((documents) => {
-    console.log("Data Retrieved : " + documents);
-    fetchedEmployees = documents;
-    return Employee.count();
-  }).then(count => {
-    response.status(200).json({
-      message: "Records retrieved successfully",
-      status: "OK",
-      employees: fetchedEmployees,
-      maxEmployees: count
-    });
-  })
-    .catch(error => {
-      response.status(500).json({
-        message: 'Fetching Information Process Failed!!!'
-      });
-    });
-});
+router.get("", employeeController.getEmployees);
 
-router.get("/:employeeId", (request, response, next) => {
-  Employee.findById(request.params.employeeId)
-    .then(employee => {
-      if (employee) {
-        response.status(200).json(employee);
-      } else {
-        response.status(404).json({
-          message: "Records Not Found"
-        });
-      }
-    })
-    .catch(error => {
-      response.status(500).json({
-        message: 'Fetching Information Process Failed!!!'
-      });
-    });
-});
+router.get("/:employeeId", employeeController.getEmployee);
 
-router.delete("/:employeeId",
-  checkAuth,
-  (request, response, next) => {
-    console.log("Employee Id : " + request.params.employeeId);
-    Employee.deleteOne({ _id: request.params.employeeId, creator: request.userData.userId })
-      .then(result => {
-        if (result.n > 0) {
-          response.status(200).json({
-            message: "Record Deleted Successfully",
-            status: "OK"
-          });
-        }
-        else {
-          response.status(401).json({
-            message: "Not Authorized to Delete",
-            status: "BAD AUTHENTICATION REQUEST"
-          });
-        }
-      })
-      .catch(error => {
-        response.status(500).json({
-          message: 'Deleting Information Process Failed!!!'
-        });
-      });
-  });
+router.delete("/:employeeId", checkAuth, employeeController.deleteEmployee);
 
 module.exports = router;
 
