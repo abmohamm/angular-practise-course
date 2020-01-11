1991b69e6de28b3a8de8514ab38e00cbc1e8045a
abmohamm <mohd.92java@gmail.com>
2020-01-11 08:36:45 -0500

    Adding logics to separate middleware

--- node-backend-app/routes/employees.js
+++ node-backend-app/routes/employees.js
@@ -1,37 +1,13 @@
 const express = require("express");
-const multer = require("multer");
 const checkAuth = require("../middle-ware/check-auth");
+const extractMiddlewareFile = require("../middle-ware/middlewarefile");
+
 const router = express.Router();
 const employeeController = require("../controllers/employees");
 
-const MIME_TYPE_MAP = {
-  'image/png': 'png',
-  'image/jpeg': 'jpg',
-  'image/jpg': 'jpg'
-};
+router.post("", checkAuth, extractMiddlewareFile, employeeController.createEmployee);
 
-const storage = multer.diskStorage({
-  destination: (request, file, callback) => {
-    const isValid = MIME_TYPE_MAP[file.mimetype];
-    let error = new Error("Invalid Mime Type");
-    if (isValid) {
-      error = null;
-    }
-    callback(error, "node-backend-app/images");
-  },
-  filename: (request, file, callback) => {
-    const name = file.originalname.toLowerCase().split(' ').join('-');
-    const extension = MIME_TYPE_MAP[file.mimetype];
-    callback(null, name + '-' + Date.now() + '.' + extension);
-  }
-});
-
-
-router.post("", checkAuth,
-                multer({ storage: storage }).single("image"), employeeController.createEmployee);
-
-router.put("/:employeeId", checkAuth,
-                           multer({ storage: storage }).single("image"), employeeController.updateEmployee);
+router.put("/:employeeId", checkAuth, extractMiddlewareFile, employeeController.updateEmployee);
 
 router.get("", employeeController.getEmployees);
 
